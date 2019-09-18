c8b95e3fbaded6f83812722c12c7a3247f3c2b40
Abdulrouf <roufy235@gmail.com>
2019-09-18 21:42:51 +0100

    beta phase3

--- app/src/main/java/com/covirtue/hng/controllers/SignUpActivity.kt
+++ app/src/main/java/com/covirtue/hng/controllers/SignUpActivity.kt
@@ -3,6 +3,7 @@ package com.covirtue.hng.controllers
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Patterns
 import android.view.View
 import android.widget.Toast
 import com.covirtue.hng.R
@@ -29,19 +30,23 @@ class SignUpActivity : AppCompatActivity() {
                     if (regPassword.text.isNotEmpty()) {
                         if (regConfirmPassword.text.toString().isNotEmpty()) {
                             if (regPassword.text.toString() == regConfirmPassword.text.toString()) {
-                                val data = LoginModel(
-                                    regName.text.toString(),
-                                    regUsername.text.toString(),
-                                    regEmail.text.toString(),
-                                    regPassword.text.toString()
-                                )
-                                DataServices.userDataGlobal = data
-                                if (DataServices.saveLoginDetails(this, true)) {
-                                    Toast.makeText(this, "Account successfully created", Toast.LENGTH_LONG).show()
-                                    startActivity(Intent(this, MainActivity::class.java))
-                                    finish()
+                                if (Patterns.EMAIL_ADDRESS.matcher(regEmail.text.toString()).matches()) {
+                                    val data = LoginModel(
+                                        regName.text.toString(),
+                                        regUsername.text.toString(),
+                                        regEmail.text.toString(),
+                                        regPassword.text.toString()
+                                    )
+                                    DataServices.userDataGlobal = data
+                                    if (DataServices.saveLoginDetails(this, true)) {
+                                        Toast.makeText(this, "Account successfully created", Toast.LENGTH_LONG).show()
+                                        startActivity(Intent(this, MainActivity::class.java))
+                                        finish()
+                                    } else {
+                                        Toast.makeText(this, "unable to create account", Toast.LENGTH_LONG).show()
+                                    }
                                 } else {
-                                    Toast.makeText(this, "unable to create account", Toast.LENGTH_LONG).show()
+                                    Toast.makeText(this, "Invalid email address", Toast.LENGTH_LONG).show()
                                 }
                             } else {
                                 Toast.makeText(this, "Password mismatch", Toast.LENGTH_LONG).show()
